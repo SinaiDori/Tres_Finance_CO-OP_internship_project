2eda8ea72f47182e1f1ba12b43b9f48d8571fcfa
Sinay Dori <dori.sinay@post.runi.ac.il>
2025-08-25 12:38:11 +0300

    Testing multiple runs with the Github action

--- main_sub_scan.py
+++ main_sub_scan.py
@@ -136,7 +136,7 @@ async def run_multiple_keys(n: int = 1):
 
 if __name__ == "__main__":
     try:
-        asyncio.run(run_multiple_keys(1))
+        asyncio.run(run_multiple_keys(2))
     except KeyboardInterrupt:
         print("\n🛑 Process interrupted by user.")
 
