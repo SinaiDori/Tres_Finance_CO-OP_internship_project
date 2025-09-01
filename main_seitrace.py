bcebc7c8d3761175d4c1287bf5e7055b0b759c01
Sinay Dori <dori.sinay@post.runi.ac.il>
2025-09-01 17:06:20 +0300

    Testing 10 continues runs of seitrace

--- main_seitrace.py
+++ main_seitrace.py
@@ -128,6 +128,6 @@ async def run_multiple_keys(n: int = 1):
 
 if __name__ == "__main__":
     try:
-        asyncio.run(run_multiple_keys(2))
+        asyncio.run(run_multiple_keys(10))
     except KeyboardInterrupt:
         print("\n🛑 Process interrupted by user.")
