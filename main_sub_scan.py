9933bf8454198162418e7c4374180835021ca89c
Sinay Dori <dori.sinay@post.runi.ac.il>
2025-09-04 09:34:22 +0300

    Testing 10 continues runs of subscan

--- main_sub_scan.py
+++ main_sub_scan.py
@@ -190,7 +190,7 @@ async def run_multiple_keys(n: int = 1):
 
 if __name__ == "__main__":
     try:
-        asyncio.run(run_multiple_keys(2))
+        asyncio.run(run_multiple_keys(10))
     except KeyboardInterrupt:
         print("\n🛑 Process interrupted by user.")
 
